ea9b334474e02b2262a0f7d23be789d3e31fa527
Your Name <you@example.com>
2026-04-01 23:38:55 +0530

    small change(4) to the support message

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.b6nt5mqu608"
+    "revision": "0.n4fo0hmp9j8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
