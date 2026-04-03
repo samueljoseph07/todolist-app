7e032d1ea54d91009388137442fac0b0b537cddd
Your Name <you@example.com>
2026-04-03 22:47:18 +0530

    modify message(11)

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.klh84m4g184"
+    "revision": "0.j43fak2vhnk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
