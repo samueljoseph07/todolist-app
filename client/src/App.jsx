c0ce3c49e8f8ab559906afa1882e3558525025c4
samueljoseph07 <153818301+samueljoseph07@users.noreply.github.com>
2026-04-05 12:15:02 +0530

    Update motivational message in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -201,7 +201,7 @@ export default function App() {
         </button>
         </div>
       </header>
-      {/* <p className="pt-0 pb-2 px-6 flex justify-between items-center z-10">You're doing great! Hope you have an amazing day!</p> */}
+      <p className="pt-0 pb-2 px-6 flex justify-between items-center z-10">You've done a great job yesterday! Keep going!!</p>
 
       <main className="flex-1 px-4 pb-40">
         {loading ? (
