9a95fd7acba6a661af2dc8000a24e7cdc68dbdd1
Samuel <josephsamuel1236@gmail.com>
2026-04-10 02:22:06 +0530

    remove the support message

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -202,7 +202,7 @@ export default function App() {
         </button>
         </div>
       </header>
-      <p className="pt-0 pb-2 px-6 flex justify-between items-center z-10 italic">The AI model is up and running :)</p>
+      <p className="pt-0 pb-2 px-6 flex justify-between items-center z-10 italic"></p>
 
       <main className="flex-1 px-4 pb-40">
         {loading ? (
